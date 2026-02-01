6da4e537565692342e3a480ace7cc2d48c2d2f8e
Ken Fehling <1200759+kenfehling@users.noreply.github.com>
2026-01-31 15:14:56 -1000

    Revert "Debug DSL"
    This reverts commit a99feff54a134ddb7ac7b3b9ae2e2d73b5af5fe4.

--- src/strategy-dsl.js
+++ src/strategy-dsl.js
@@ -222,9 +222,6 @@ export class StrategyDSL {
 
       for (const component of allComponents) {
         const score = executeComponent(component, game, grid);
-        if (typeof window !== 'undefined' && window.debugDSL) {
-          console.log(`  Component ${component.name}: +${score}`);
-        }
         totalScore += score;
       }
 
